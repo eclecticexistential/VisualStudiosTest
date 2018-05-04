b6e6d36dfb9f674ccfe4cb8b60640da3b15f550b
eclecticexistential <allofitbra@gmail.com>
2018-05-03 23:19:13 -0700

    Following Treehouse, used null as validation check even though professional programmers highly recommend never doing this.

--- VSTestCode/VSTestCode/Program.cs
+++ VSTestCode/VSTestCode/Program.cs
@@ -14,7 +14,25 @@ namespace VSTestCode
             Map map = new Map(8, 5);
             try
             {
-                MapLocation mapLocation = new MapLocation(20, 20, map);
+                Path path = new Path(
+                    new []{
+                    new MapLocation(0, 2, map),
+                    new MapLocation(1, 2, map),
+                    new MapLocation(2, 2, map),
+                    new MapLocation(3, 2, map),
+                    new MapLocation(4, 2, map),
+                    new MapLocation(5, 2, map),
+                    new MapLocation(6, 2, map),
+                    new MapLocation(7, 2, map),
+                });
+                
+                MapLocation location = path.GetLocationAt(0);
+
+                if (location != null)
+                {
+                    Console.WriteLine(location.X + "," + location.Y);
+                }
+                
             }
             catch (OutOfBoundsException ex)
             {
@@ -24,9 +42,9 @@ namespace VSTestCode
             {
                 Console.WriteLine("unhandled program exception here.");
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                Console.WriteLine("Unhandled Exception");
+                Console.WriteLine("Unhandled Exception: " + ex);
             }
         }
     }
