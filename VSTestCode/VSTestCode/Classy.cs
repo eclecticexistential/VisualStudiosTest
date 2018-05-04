faa08328f690087ac910ea303d6a0977369ffb88
eclecticexistential <allofitbra@gmail.com>
2018-05-04 15:41:35 -0700

    Learning the difference between when to use properties or methods

--- VSTestCode/VSTestCode/Classy.cs
+++ VSTestCode/VSTestCode/Classy.cs
@@ -8,6 +8,22 @@ namespace VSTestCode
 {
     class Invader
     {
-        public MapLocation Location { get; private set; }
+        private readonly Path _path;
+        private int _pathStep = 0;
+        public MapLocation Location
+        {
+            get
+            {
+                return _path.GetLocationAt(_pathStep);
+            }
+        }
+        public Invader(Path path)
+        {
+            _path = path;
+        }
+        public void Move()
+        {
+            _pathStep += 1;
+        }
     }
 }
