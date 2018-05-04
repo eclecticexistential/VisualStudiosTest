56456b0b19777d742f7815ad57bc7ab75201686e
eclecticexistential <allofitbra@gmail.com>
2018-05-04 15:57:26 -0700

    Use block bodies for constructors else syntatic sugar

--- VSTestCode/VSTestCode/Classy.cs
+++ VSTestCode/VSTestCode/Classy.cs
@@ -10,20 +10,13 @@ namespace VSTestCode
     {
         private readonly Path _path;
         private int _pathStep = 0;
-        public MapLocation Location
-        {
-            get
-            {
-                return _path.GetLocationAt(_pathStep);
-            }
-        }
+        public MapLocation Location => _path.GetLocationAt(_pathStep);
+
         public Invader(Path path)
         {
             _path = path;
         }
-        public void Move()
-        {
-            _pathStep += 1;
-        }
+
+        public void Move() => _pathStep += 1;
     }
 }
