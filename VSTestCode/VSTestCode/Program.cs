27015c83aefa34a6ae2cf2fc9943d71999f041c4
eclecticexistential <allofitbra@gmail.com>
2018-05-03 19:00:59 -0700

    Making point reusable for OOP

--- VSTestCode/VSTestCode/Program.cs
+++ VSTestCode/VSTestCode/Program.cs
@@ -8,13 +8,13 @@ namespace VSTestCode
 {
     class Game
     {
-        static void Main()
+        public static void Main()
         {
             Tower tower = new Tower();
-            Map map = new Map();
-            map.Width = 8;
-            map.Height = 5;
+            Map map = new Map(8, 5);
+            Point point = new Point(4, 2);
 
+            Console.WriteLine(point.DistanceTo(5, 5));
         }
     }
 }
